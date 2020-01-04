ab6547f690930dfc7c3864f48d713d1bf2402956
Vu Nguyen <tranvu.t1.bkdn@gmail.com>
2020-01-04 22:13:35 +0700

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,7 +25,8 @@ function Login(props) {
             props.history.push('/');
         },
         onError(err) {
-            setErrors(err.graphQLErrors[0].extensions.exception.errors);
+            // setErrors(err.graphQLErrors[0].extensions.exception.errors);
+            console.log(err)
           },
         variables: values
     });
